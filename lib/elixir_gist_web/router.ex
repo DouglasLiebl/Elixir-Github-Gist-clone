70427dacf0e232a0a0e8b9da05cbaf696560b7a4
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2024-01-02 21:05:46 -0300

    Add: phoenix generated authentication

--- lib/elixir_gist_web/router.ex
+++ lib/elixir_gist_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ElixirGistWeb.Router do
   use ElixirGistWeb, :router
 
+  import ElixirGistWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ElixirGistWeb.Router do
     plug :put_root_layout, html: {ElixirGistWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule ElixirGistWeb.Router do
 
       live_dashboard "/dashboard", metrics: ElixirGistWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ElixirGistWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ElixirGistWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/tb_users/register", UserRegistrationLive, :new
+      live "/tb_users/log_in", UserLoginLive, :new
+      live "/tb_users/reset_password", UserForgotPasswordLive, :new
+      live "/tb_users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/tb_users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ElixirGistWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ElixirGistWeb.UserAuth, :ensure_authenticated}] do
+      live "/tb_users/settings", UserSettingsLive, :edit
+      live "/tb_users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ElixirGistWeb do
+    pipe_through [:browser]
+
+    delete "/tb_users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ElixirGistWeb.UserAuth, :mount_current_user}] do
+      live "/tb_users/confirm/:token", UserConfirmationLive, :edit
+      live "/tb_users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
